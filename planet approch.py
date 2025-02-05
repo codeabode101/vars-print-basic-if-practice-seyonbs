36e5cb6c6e0abb032792b30d14d8a7462cfa0219
seyonbs <seyonbs@gmail.com>
2025-02-04 21:32:15 -0500

    Add files via upload

---
+++ planet approch.py
@@ -0,0 +1,13 @@
+newplanet = "mars"
+astruanaut = "seyon"
+rocket = "saturn v"
+rocketfuel = 100
+if newplanet == "mars":
+    print("you see craters")
+else:
+    print("you are not in the correct planet")    
+if rocketfuel<100:
+    print("warning:low fuel for landing!")
+else:
+    rocketfuel == 100
+    print("fuel level is good for landing")
---
+++ space explorer.py
@@ -0,0 +1,10 @@
+astronuatname =  "balaji"
+rocketfuel =10
+startingplanet = "venus"
+if rocketfuel == 100:     
+    print("Fuel tank is full! Ready for launch!")
+elif rocketfuel > 100:
+     print("Fuel tank is overfulled!check before launch!")
+else:
+    print("Fuel tank is not full. Check before launch!")
+    
