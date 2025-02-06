a77ed77e0575be77b328ecaee36827d4fbe813f3
seyonbs <seyonbs@gmail.com>
2025-02-05 20:29:24 -0500

    Add files via upload

---
+++ asteroid field.py
@@ -0,0 +1,10 @@
+choice = "dodge"
+if choice == "dodge":
+    print("dodged!fuel -10%")
+else: 
+    print("lasers fired!-30%")
+rocketfuel = 1
+if rocketfuel<20:
+    print("critical warning:fuel very low")
+else:
+    print("fuel level is stable")      
