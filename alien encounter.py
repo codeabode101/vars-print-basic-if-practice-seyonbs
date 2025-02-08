904e69cbe7660a947332c228b4d2e2b7fd345d0c
seyonbs <seyonbs@gmail.com>
2025-02-07 19:56:52 -0500

    Add files via upload

---
+++ alien encounter.py
@@ -0,0 +1,10 @@
+alien_mood = "friendly"
+if alien_mood == "friendly":
+    print("got space crystals! +20%","fuel")
+else:
+    print("alien attack! -25","%", "fuel")
+rocketfuel = 100 
+if rocketfuel<75:
+    print("fuel tank is not full yet")
+else:
+    print("fuel is almost full!")            
