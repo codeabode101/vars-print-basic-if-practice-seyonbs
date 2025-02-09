6b6e336874ee96ae984d2dfca65669a29a5693de
seyonbs <seyonbs@gmail.com>
2025-02-08 19:29:00 -0500

    Add files via upload

---
+++ mission succses.py
@@ -0,0 +1,11 @@
+rocketfuel = 100
+name = "seyon"
+planet = "mars"
+if rocketfuel>50:
+    print(name, "reaches" , planet , "safely.")
+else:
+    print(name, "needs rescue from" , planet , "orbit")
+if planet == "mars":
+    print("you have made it to mars")
+else:
+    print("you explored a new world")    
